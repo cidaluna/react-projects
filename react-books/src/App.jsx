cb20bf26c227d7464597dee1147f39c4716bd3a4
Cida Luna <cidalunna@hotmail.com>
2024-09-18 17:03:04 -0300

    Created global style and import Google font

--- react-books/src/App.jsx
+++ react-books/src/App.jsx
@@ -5,10 +5,6 @@ const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(180deg, #09117E, #09ABF3);
-
-li{
-  list-style: none;
-}
 `
 
 function App() {
--- react-books/src/main.jsx
+++ react-books/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createGlobalStyle } from 'styled-components'
 import App from './App.jsx'
-import './index.css'
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap');
+
+body{
+  display: flex;
+  margin: 0;
+  padding: 0;
+  font-family: "Montserrat", system-ui;
+  font-style: normal;
+}
+
+li{
+  list-style: none;
+}
+`
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyle/>
     <App />
   </StrictMode>,
 )
